47134c032a233d4a91815eff66d6204a8e763019
Alexander Chistyakov <al.ol.chistyakov@gmail.com>
2016-08-14 01:12:22 +0300

    added skeleton for test

---
+++ yandex_mail_test.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+USERNAME = "test.test100500"
+PASSWORD = "Passwo1!"
+
+
+class TestSendMessage(TestCase):
+    def setUp(self):
+        self.driver = webdriver.Remote(
+            command_executor='http://192.168.99.100:32769/wd/hub',
+            desired_capabilities=DesiredCapabilities.FIREFOX)
+
+        self.yandex_mail = YandexMailWrapper(self.driver)
+        self.yandex_mail.login(USERNAME, PASSWORD)
+
+    def test_receiver_subject_body_exists(self):
+        self.yandex_mail.clear_list_of_sent_messages()
+        self.yandex_mail.send_new_message(to="test.test100500@yandex.ru", subject="test", body="how you doin?")
+        last_sent_message = self.yandex_mail.sent_messages.next()
+        self.assertEqual(last_sent_message.to, "test.test100500@yandex.ru")
+        self.assertEqual(last_sent_message.subject, "test")
+        self.assertEqual(last_sent_message.body, "how you doin?")
+
+    def tearDown(self):
+        self.driver.quit()
